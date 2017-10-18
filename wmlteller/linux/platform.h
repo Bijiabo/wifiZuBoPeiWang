091a9b070e4ad189f2662de91d96636e9c7619c3
牧梦追马 <420260138@qq.com>
2017-10-19 00:48:21 +0800

    新建 platform.h

---
+++ wmlteller/linux/platform.h
@@ -0,0 +1,16 @@
+/*
+ * WML Teller platform definitions
+ * Copyright (c) 2016, Shoowing <420260138@qq.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of Apache License Version 2.0
+ *
+ * See README for more details.
+ */
+#ifndef PLATFORM_H
+#define PLATFORM_H
+
+
+#define WML_Printf  printf
+
+#endif // PLATFORM_H
